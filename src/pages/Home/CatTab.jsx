646c5e44e664badb376f31428ff0eb34b9e41ed3
AbidShazid3 <abidshazid3@gmail.com>
2024-06-03 13:33:00 +0600

    added cat tab video

--- src/pages/Home/CatTab.jsx
+++ src/pages/Home/CatTab.jsx
@@ -5,6 +5,10 @@ import PackageCard from './PackageCard';
 import { Link } from 'react-router-dom';
 import TourGuidesCards from './TourGuides/TourGuidesCards';
 import Heading from '../Shared/Heading/Heading';
+import video1 from '/ranga.mp4';
+import video2 from '/cox.mp4';
+import video3 from '/martin.mp4';
+
 
 const CatTab = () => {
     const [activeTab, setActiveTab] = useState(0);
@@ -30,10 +34,20 @@ const CatTab = () => {
                 <TabPanel>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-5 p-2'>
                         <div>
-
+                            <video width="560" height="315" controls>
+                                <source src={video1} type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
                         </div>
+                        <video width="560" height="315" controls>
+                            <source src={video2} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
                         <div>
-
+                            <video width="560" height="315" controls>
+                                <source src={video3} type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
                         </div>
                         <div>
 
